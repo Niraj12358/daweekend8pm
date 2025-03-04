85bc9ec020d44caed8e7271d0f95905a5fa8e3b3
Niraj12358 <pandeyniraj614@gmail.com>
2025-03-04 11:44:23 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,12 @@
 print("hello")
+
+def add(*a):
+   # l=len(a)
+    #sum =0
+    #for i in range (0,l):
+       # if(i%2==0):
+          #  sum =a[i] +sum
+
+
+    #print(a,sum)
+# add(12,34,54,46,67)
