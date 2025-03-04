b18b8b805086bc54c156dec16a2c1f420a10c8f4
Niraj12358 <pandeyniraj614@gmail.com>
2025-03-04 11:51:01 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,4 +1,16 @@
-print("hello")
+=====TUPPLE FUCTION FOR SUMATION======
+
+ef add(*tup):
+ #   l= len(tup)
+  #  sum =0
+   # for i in range(0,l):
+     #   sum=sum+tup[i]
+  # ''' print(tup,sum)
+#add(12,13,14)
+#add(25,12,31,65,)
+#add(55,2,12,50,45,65,25,)'''
+
+======TUPPLE FUNCTION FOR EVEN NUMBER======
 
 def add(*a):
    # l=len(a)
@@ -10,3 +22,32 @@ def add(*a):
 
     #print(a,sum)
 # add(12,34,54,46,67)
+
+=====TUPPLE FUCTION FOR SUM OF EVEN NUMBER======
+
+#def sumEle(*a):
+   # sum=0
+   # for i in range (0,len(a)):
+       # if(a[i]%3==0 and a[i]%5==0 and a[i]%7==0):
+           #sum= sum+  a[i]
+
+        #else:
+             # pass
+    #print (a,  sum)
+
+#sumEle(105,67,34,90,210 )
+
+==== TUPPLE FUNCTION FOR ODD NUMBER =====
+
+def sumEle(*a):
+    #sum=0
+   # for i in range (0,len(a)):
+      ##   if(a[i]%3==0 and a[i]%5==0 and a[i]%7==0):
+           #sum= sum+  a[i]
+
+         #else:
+             # pass
+    #print (a,  sum)
+
+#sumEle(105,67,34,90,210 )
+
